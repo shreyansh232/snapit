fe4d4a9b8aa4a70cdf797c8e5466a5e84813697f
shreyansh-web <shreyansh487@gmail.com>
2025-07-07 13:08:25 +0530

    feat: add videocard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
     <main className="wrapper page">
       <Header title="All Videos" subHeader="Public Library" />
-      <h1>Welcome to snapit</h1>
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   );
 };
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 import React from "react";
 
 const page = async ({ params }: ParamsWithSearch) => {
@@ -11,7 +13,11 @@ const page = async ({ params }: ParamsWithSearch) => {
         title="Shreyansh Pandey"
         userImg="/assets/images/dummy.jpg"
       />{" "}
-      <h1 className="text-2xl font-karla">User Id: {id}</h1>
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </div>
   );
 };
